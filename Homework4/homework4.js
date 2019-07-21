7108472504d640f0310492ce8e57bb423c6f2c23
OksanaTukach <Oshabovta@mail.ru>
2019-07-22 02:18:58 +0300

    add heroku

--- Homework4/homework4.js
+++ Homework4/homework4.js
@@ -2,16 +2,16 @@
 var btn = document.querySelector('#btn');
 
 btn.addEventListener('click', function(event) {
-    var newUser = {email: 'okstukach@gmail.com'};
-    var activeUser = { id: '5d34ee13d4279300044487e7'};
-    var signInReq = {email: 'okstukach@gmail.com' , password: 'sw8rMg'};
-    doRequest(signInReq);
+    //var newUser = {email: 'okstukach@gmail.com'};
+    //var activeUser = { id: '5d34ee13d4279300044487e7'};
+    //var signInReq = {email: 'okstukach@gmail.com' , password: 'sw8rMg'};
+    doRequest({});
 });
 
 function doRequest(data) {
-    fetch('https://intern-staging.herokuapp.com/api/identification/sign_in', {
-        method: 'POST',
-        body: JSON.stringify(data),
+    fetch('https://intern-staging.herokuapp.com/api/identification', {
+        method: 'GET',
+        //body: JSON.stringify(data),
         headers: {
             'Content-type': 'application/json',
         },
