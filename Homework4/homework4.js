ec33c21226498fe0a9c3247a7b3680c36ae27d5e
OksanaTukach <Oshabovta@mail.ru>
2019-07-22 19:32:19 +0300

    add heroku

--- Homework4/homework4.js
+++ Homework4/homework4.js
@@ -4,15 +4,16 @@ var btn = document.querySelector('#btn');
 btn.addEventListener('click', function(event) {
     var newUser = {email: 'okstukach@gmail.com'};
     var activeUser = { id: '5d34ee13d4279300044487e7'};
-    var tokenStr = {token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzRlZTEzZDQyNzkzMDAwNDQ0ODdlNyIsImlhdCI6MTU2Mzc5NDA2NywiZXhwIjoxNTYzNzk3NjY3fQ.i8QSZtlJPcMewoUrmpjFp_REXr--jE_nV-_td27Lalk'};
+    var tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzRlZTEzZDQyNzkzMDAwNDQ0ODdlNyIsImlhdCI6MTU2Mzc5NDA2NywiZXhwIjoxNTYzNzk3NjY3fQ.i8QSZtlJPcMewoUrmpjFp_REXr--jE_nV-_td27Lalk';
     var signInReq = {email: 'okstukach@gmail.com' , password: '6s1Ms6'};
-    doRequest(signInReq, tokenStr);
+    var comment = {message: "Hello it", parentId: "test-234"};
+    doRequest(comment, tokenStr);
 });
 
 function doRequest(data, token) {
     fetch('https://intern-staging.herokuapp.com/api/comment', {
-        method: 'GET',
-        //body: JSON.stringify(data),
+        method: 'POST',
+        body: JSON.stringify(data),
         headers: {
             'Content-type': 'application/json',
             'token': token
