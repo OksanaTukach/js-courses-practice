c3c75367d241f4b2def5d92ccd673ecdf003a409
OksanaTukach <Oshabovta@mail.ru>
2019-07-23 01:15:43 +0300

    add heroku

--- Homework4/homework4.js
+++ Homework4/homework4.js
@@ -2,12 +2,12 @@
 var btn = document.querySelector('#btn');
 
 btn.addEventListener('click', function(event) {
-   // var newUser = {email: 'okstukach@gmail.com'};
-    //var activeUser = { id: '5d34ee13d4279300044487e7'};
-   // var tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzRlZTEzZDQyNzkzMDAwNDQ0ODdlNyIsImlhdCI6MTU2MzgyMzA4MSwiZXhwIjoxNTYzODI2NjgxfQ.0IzL8UtVhOwsK1qWpzwXrq7OrGFoFRZkvscE0uw-tSc';
-   var signInReq = {email: 'okstukach@gmail.com' , password: '6s1Ms6'};
-   // var comment = {message: "Hello it", parentId: "test-234"};
-    doRequest({signInReq});
+    var newUser = {email: 'okstukach@gmail.com'};
+    var activeUser = { id: '5d34ee13d4279300044487e7'};
+    //var tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzRlZTEzZDQyNzkzMDAwNDQ0ODdlNyIsImlhdCI6MTU2MzgxNDMyNSwiZXhwIjoxNTYzODE3OTI1fQ.xxbQhOoFgciKKCDe3dcEXvUHqmQ_SJwbuUs511mEoz0';
+    var signInReq = {email: 'okstukach@gmail.com' , password: '6s1Ms6'};
+    //var comment = {message: "Hello it", parentId: "test-234"};
+    doRequest(signInReq);
 });
 
 function doRequest(data) {
