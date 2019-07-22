c58cd8a19141406c0337b95968794f0fb33f29c4
OksanaTukach <Oshabovta@mail.ru>
2019-07-23 01:33:12 +0300

    add heroku

--- Homework4/homework4.js
+++ Homework4/homework4.js
@@ -1,21 +1,12 @@
 
-var btn = document.querySelector('#btn');
+var tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzRlZTEzZDQyNzkzMDAwNDQ0ODdlNyIsImlhdCI6MTU2MzgzMzc5NCwiZXhwIjoxNTYzODM3Mzk0fQ.e04bLhQvxPu0sb9y0pfPtZ30Ik8NMOA4NPPRs9Hm_nU';
+var API = 'https://intern-staging.herokuapp.com/api';
 
-btn.addEventListener('click', function(event) {
-    var newUser = {email: 'okstukach@gmail.com'};
-    var activeUser = { id: '5d34ee13d4279300044487e7'};
-    //var tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzRlZTEzZDQyNzkzMDAwNDQ0ODdlNyIsImlhdCI6MTU2MzgxNDMyNSwiZXhwIjoxNTYzODE3OTI1fQ.xxbQhOoFgciKKCDe3dcEXvUHqmQ_SJwbuUs511mEoz0';
-    var signInReq = {email: 'okstukach@gmail.com' , password: '6s1Ms6'};
-    //var comment = {message: "Hello it", parentId: "test-234"};
-    doRequest(signInReq);
-});
-
-function doRequest(data) {
-    fetch('https://intern-staging.herokuapp.com/api/identification/sign_in', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-type': 'application/json',
+function doRequest(url, method, data, headers) {
+    fetch('${API}${url}', {
+        method: method,
+        body: data,
+        headers: headers ,
         },
     }) .then(
         resp => resp.json()
@@ -23,3 +14,19 @@ function doRequest(data) {
         json => console.log(json)
     );
 }
+doRequest('/identification', 'GET', null)
+  
+var form = document.forms.namedItem('fileinfo');
+form.addEventListener('submit', function (ev) {
+    var formD = new FormData(this);
+   
+    formD.append('parentEntityId', '5d34ee13d4279300044487e7');
+
+    doRequest('/file', 'POST', formD, {'token': tokenStr});
+    ev.preventDefault();
+})
+    
+
+
+
+//var activeUser = { id: '5d34ee13d4279300044487e7'};
