26b188eb7a1c5a0320c8cb74d615884afc03dcdb
OksanaTukach <Oshabovta@mail.ru>
2019-07-23 17:47:36 +0300

    add heroku

--- Homework4/homework4.js
+++ Homework4/homework4.js
@@ -9,7 +9,7 @@ function doRequest(url, method, data, headers) {
         headers: headers ,
     
     }) .then(
-        resp => resp.json(),
+        resp => resp.json()
     ) .then(
         json => console.log(json)
     );
